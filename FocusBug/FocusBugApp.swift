132b5db60bcf9bbed277c7db6c86bff0bd5577c0
Jason Ji <uberjason@gmail.com>
2021-07-16 13:55:46 -0400

    Initial Commit

---
+++ FocusBug/FocusBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusBugApp.swift
+//  FocusBug
+//
+//  Created by Jason on 7/16/21.
+//
+
+import SwiftUI
+
+@main
+struct FocusBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
