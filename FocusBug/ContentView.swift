09b136f422c918146e4a98c15ff1601747cf6ae5
Jason Ji <uberjason@gmail.com>
2021-07-16 16:16:24 -0400

    Show the bug

--- FocusBug/ContentView.swift
+++ FocusBug/ContentView.swift
@@ -8,9 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    enum Field: Int, Hashable {
+        case first, second
+    }
+    
+    @State var text: String = ""
+    @FocusState var focused: Field?
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("""
+Instructions:
+1. Click in the Foo text field, and observe the console prints "FOCUSBUG: change: Optional(FocusBug.ContentView.Field.first)". This is correct behavior.
+2. Click in the Bar text field, and observe the console prints "FOCUSBUG: change: Optional(FocusBug.ContentView.Field.second)". This is correct behavior.
+3. From either text field, hit TAB from the keyboard. Observe that the text field loses focus, but focus doesn't go to the other text field, and also there's no print statement for change in the focused value.
+""")
+            List {
+                TextField("Foo", text: $text)
+                    .onSubmit {
+                        print("FOCUSBUG: submit foo")
+                    }
+                    .padding()
+                    .focused($focused, equals: .first)
+                
+                TextField("Bar", text: $text)
+                    .onSubmit {
+                        print("FOCUSBUG: submit bar")
+                    }
+                    .padding()
+                    .focused($focused, equals: .second)
+                
+                Button("Done") {
+                    print("FOCUSBUG: done")
+                }
+            }
+            .onChange(of: focused) { newValue in
+                print("FOCUSBUG: change: \(newValue)")
+            }
+        }
     }
 }
 
